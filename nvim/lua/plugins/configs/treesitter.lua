9f8d97820567c391455a48482b113ba07d6bf212
Daniel Hauge <animcuil@gmail.com>
2023-09-04 18:53:43 +0200

    Added languages

--- nvim/lua/plugins/configs/treesitter.lua
+++ nvim/lua/plugins/configs/treesitter.lua
@@ -1,11 +1,27 @@
 local options = {
-  ensure_installed = { "lua" },
-
+  ensure_installed = {
+    "lua",
+    "bash",
+    "c",
+    "comment",
+    "css",
+    "go",
+    "graphql",
+    "help",
+    "html",
+    "http",
+    "json",
+    "latex",
+    "lua",
+    "python",
+    "rust",
+  },
+  -- Set parser install dir
   highlight = {
     enable = true,
     use_languagetree = true,
   },
-
+  sync_install = false,
   indent = { enable = true },
 }
 
