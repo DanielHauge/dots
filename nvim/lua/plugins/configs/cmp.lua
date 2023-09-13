de472f9c27c888a97f7a0893cc4df460b8b40dae
Daniel Hauge <animcuil@gmail.com>
2023-09-13 18:38:48 +0200

    Added close selection on esc

--- nvim/lua/plugins/configs/cmp.lua
+++ nvim/lua/plugins/configs/cmp.lua
@@ -70,13 +70,14 @@ local options = {
 
   mapping = {
     -- Use arrows to navigate the completion menu
-    ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+    ["<Down>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
     ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
+    ["<Esc>"] = cmp.mapping.close(),
     ["<C-e>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Insert,
