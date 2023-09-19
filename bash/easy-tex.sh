af314296b3d7125f1913a9c502a4807c37919741
Daniel Hauge <animcuil@gmail.com>
2023-09-19 20:17:09 +0200

    Improved tex

--- bash/easy-tex.sh
+++ bash/easy-tex.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-tex() {
+tex(){
+    if [ $# -eq 0 ]; then
+        local file=$(ls *.tex | head -n 1)
+        buildtex $file
+    
+    elif [ $# -eq 1 ]; then
+
+        if [ $1 == "-h" ]; then
+            echo "Usage: tex [file.tex]"
+            return 1
+        fi
+
+        local file=$1
+        buildtex $file
+
+    else
+        echo "Usage: tex [file.tex]"
+        return 1
+    fi
+}
+
+buildtex() {
     if [ $# -eq 0 ]; then
         echo "Usage: easy-tex template.tex"
         return 1
