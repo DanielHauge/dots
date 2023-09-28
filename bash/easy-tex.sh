3193877a08bd48b7cf3d59b515e8a6373f1d3876
Daniel Hauge <animcuil@gmail.com>
2023-09-28 18:47:18 +0200

    dos2unix shell scripts

--- bash/easy-tex.sh
+++ bash/easy-tex.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-tex(){
-    if [ $# -eq 0 ]; then
-        local file=$(ls *.tex | head -n 1)
-        buildtex $file
-    
-    elif [ $# -eq 1 ]; then
+tex() {
+	if [ $# -eq 0 ]; then
+		local file=$(ls *.tex | head -n 1)
+		buildtex $file
 
-        if [ $1 == "-h" ]; then
-            echo "Usage: tex [file.tex]"
-            return 1
-        fi
+	elif [ $# -eq 1 ]; then
 
-        local file=$1
-        buildtex $file
+		if [ $1 == "-h" ]; then
+			echo "Usage: tex [file.tex]"
+			return 1
+		fi
 
-    else
-        echo "Usage: tex [file.tex]"
-        return 1
-    fi
+		local file=$1
+		buildtex $file
+
+	else
+		echo "Usage: tex [file.tex]"
+		return 1
+	fi
 }
 
 buildtex() {
-    if [ $# -eq 0 ]; then
-        echo "Usage: easy-tex template.tex"
-        return 1
-    fi
-    local pdf_file=${1%.*}.pdf 
-    pdflatex --shell-escape $1 && latexmk -c && x $pdf_file
+	if [ $# -eq 0 ]; then
+		echo "Usage: easy-tex template.tex"
+		return 1
+	fi
+	local pdf_file=${1%.*}.pdf
+	pdflatex --shell-escape $1 && latexmk -c && x $pdf_file
 }
