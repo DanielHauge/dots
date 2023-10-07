69fc3513efcbecbc59c805f61f1b9a45caea68e0
Daniel Hauge <animcuil@gmail.com>
2023-10-07 21:47:35 +0200

    Scroll plugin

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -3,77 +3,77 @@ local overrides = require "custom.configs.overrides"
 ---@type NvPluginSpec[]
 local plugins = {
 
-  {
-    "b0o/schemastore.nvim",
-  },
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
-  },
-  {
-    "lukas-reineke/lsp-format.nvim",
-    dependencies = { "neovim/nvim-lspconfig" },
-  },
-  -- Override plugin definition options
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
+    {
+        "b0o/schemastore.nvim",
+    },
+    {
+        "pmizio/typescript-tools.nvim",
+        dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+        opts = {},
+    },
+    {
+        "lukas-reineke/lsp-format.nvim",
+        dependencies = { "neovim/nvim-lspconfig" },
+    },
+    -- Override plugin definition options
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            -- format & linting
+            {
+                "jose-elias-alvarez/null-ls.nvim",
+                config = function()
+                    require "custom.configs.null-ls"
+                end,
+            },
+        },
         config = function()
-          require "custom.configs.null-ls"
+            require "plugins.configs.lspconfig"
+            require "custom.configs.lspconfig"
+        end, -- Override to setup mason-lspconfig
+    },
+
+    -- override plugin configs
+    {
+        "williamboman/mason.nvim",
+        opts = overrides.mason,
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = overrides.treesitter,
+    },
+
+    {
+        "nvim-tree/nvim-tree.lua",
+        -- opts = overrides.nvimtree,
+    },
+
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    {
+        "antosha417/nvim-lsp-file-operations",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-neo-tree/neo-tree.nvim",
+        },
+    },
+    { "echasnovski/mini.nvim", version = "*" },
+    -- Install a plugin
+    {
+        "max397574/better-escape.nvim",
+        event = "InsertEnter",
+        config = function()
+            require("better_escape").setup()
         end,
-      },
     },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
-
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
-
-  {
-    "nvim-tree/nvim-tree.lua",
-    -- opts = overrides.nvimtree,
-  },
-
-  {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  },
-  {
-    "antosha417/nvim-lsp-file-operations",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-neo-tree/neo-tree.nvim",
-    },
-  },
-  { "echasnovski/mini.nvim", version = "*" },
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
 }
 
 return plugins
