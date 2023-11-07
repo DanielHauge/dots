4ee9741dc3143b924cf67e545887c62d6ccffdcc
Daniel Hauge <animcuil@gmail.com>
2023-11-07 17:28:39 +0100

    Improved install.sh

--- bash/easy-rename.sh
+++ bash/easy-rename.sh
@@ -2,7 +2,7 @@
 
 mvrs() {
 	for file in *; do
-		newFile=$(echo $file | sed "s/\[//g" | sed "s/\]//g" | sed "s/ //g")
+		newFile=$(echo $file | sed "s/\[//g" | sed "s/\]//g")
 
 		mv "$file" "$newFile"
 	done
--- win/install.sh
+++ win/install.sh
@@ -6,13 +6,11 @@ choco install GoogleChrome -y
 choco install neovim -y
 choco install tree-sitter -y
 git config --global core.editor nvim
-choco install oh-my-posh -y
 choco install make -y
 choco install mingw -y
 choco install dart-sdk -y
 cohoc install flutter -y
 choco install dotnet-sdk -y
-choco install Temurin21jre -y
 choco install dotnet-runtime -y
 choco install vlc -y
 choco install wget -y
@@ -21,7 +19,6 @@ choco install cargo -y
 choco install winrar -y
 choco install docker-desktop -y
 choco install dotnet -y
-choco install wsl2 -y
 choco install 7zip -y
 choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' -y
 choco install pwsh -y
@@ -30,16 +27,16 @@ choco install nerd-fonts-JetBrainsMono -y
 choco install zig -y
 choco install golang -y
 choco install nodejs -y
-choco install windirstat -y
-choco install shellcheck -y
-choco install glow -y
-# Install python
+curl https://sh.rustup.rs -sSf | sh
 choco install python3 -y
 choco install ruff -y # fast python linter
 choco install javaruntime -y
 choco install jdk8 -y
+choco install Temurin21jre -y
+choco install windirstat -y
+choco install shellcheck -y
+choco install glow -y
 choco install pandoc -y
-curl https://sh.rustup.rs -sSf | sh
 choco install InkScape -y
 go install github.com/TheZoraiz/ascii-image-converter@latest
 # Install texlive can take time.
