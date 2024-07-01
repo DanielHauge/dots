d5b20d1e4ea7f26f9ac0796f0c42a46c6447d199
Daniel Hauge <daniel@greenwood.dk>
2024-07-01 10:03:17 +0200

    fix: Fixed nix setup script

--- nix/setup.sh
+++ nix/setup.sh
@@ -4,6 +4,6 @@ sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 sudo nix-channel --update
 sudo nixos-rebuild switch
 
-git clone https://github.com/DanielHauge/dots.git
+nix-shell -p git --run "clone https://github.com/DanielHauge/dots.git"
 sudo sed -i "/imports = \[/a \  $(pwd)/dots/nix/packs.nix" /etc/nixos/configuration.nix
 sudo nixos-rebuild switch
