98a107dd0f84dc26ac83759a0120e13b7e42dc97
Daniel Hauge <Animcuil@gmail.com>
2024-09-10 23:04:03 +0200

    fix: fixed win env

--- bash/environment.sh
+++ bash/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export GITHUB="https://github.com/DanielHauge"
 # if windows:
-if [[ "$OSTYPE" == "msys" ]]; then
+if [ -n "$WINDIR" ]; then
 	export XDG_CONFIG_HOME=$DOTS_LOC
 	export XDG_DATA_HOME=$DOTS_LOC
 	export XDG_STATE_HOME=$HOME
