079ebb95cb7856ebc0b2657acaf635ea16e10beb
Daniel Hauge <daniel@greenwood.dk>
2024-09-10 12:51:55 +0200

    fix: Only set env when windows

--- bash/environment.sh
+++ bash/environment.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export GITHUB="https://github.com/DanielHauge"
-# export XDG_CONFIG_HOME=$DOTS_LOC
-# export XDG_DATA_HOME=$DOTS_LOC
-# export XDG_STATE_HOME=$HOME
+# if windows:
+if [[ "$OSTYPE" == "msys" ]]; then
+	export XDG_CONFIG_HOME=$DOTS_LOC
+	export XDG_DATA_HOME=$DOTS_LOC
+	export XDG_STATE_HOME=$HOME
+fi
