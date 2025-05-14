9d85400c54b90721a93fc4e35f909e68ec64faec
Daniel Hauge <animcuil@gmail.com>
2025-05-14 15:51:42 +0200

    feat: improved arch install

--- arch/install.sh
+++ arch/install.sh
@@ -2,32 +2,46 @@
 
 install_zsh() {
     echo "Installing zsh..."
-    sudo pacman -S zsh
+    sudo pacman -Syuu zsh --noconfirm
     chsh -s "$(which zsh)"
 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+    sh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting'
+    sh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions'
 }
+
+if ! command -v cargo; then
+    sudo pacman -Syuu --noconfirm --needed curl base-devel
+    echo "Installing rust..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable -y
+    echo "Installing tree-sitter..."
+    sh -c "cargo install tree-sitter-cli"
+fi
 
 if ! command -v zsh; then
     install_zsh
 fi
 
 while read -r pack; do
-    if ! command -v "$pack"; then
+    if ! pacman -Q "$pack" &>/dev/null; then
         echo "Installing $pack..."
-        sudo pacman -S "$pack"
+        sudo pacman -Syuu --noconfirm "$pack"
+    else
+        echo "$pack is already installed."
     fi
 done <packages.txt
 
-if ! command -v cargo; then
-    echo "Installing rust..."
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-    rustup default stable
-fi
+(
+    cd ~/dots || exit
+    stow -R config
+)
 
-if ! command -v tree-sitter; then
-    echo "Installing tree-sitter..."
-    cargo install tree-sitter-cli
+if ! command -v paru; then
+    echo "Installing paru..."
+    (
+        cd ~ || exit
+        git clone https://aur.archlinux.org/paru.git
+        cd paru || exit
+        makepkg -si --noconfirm
+    )
 fi
