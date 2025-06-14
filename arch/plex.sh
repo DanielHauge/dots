7a6559af2c2a4b5e28d326605363ae1a1809f793
DanielHauge <animcuil@gmail.com>
2025-06-14 13:35:25 +0200

    docs: clarified about plex auth token

--- arch/plex.sh
+++ arch/plex.sh
@@ -3,3 +3,4 @@
 snap install plex-desktop
 crudini --set ~/snap/plex-desktop/common/plex.ini web myPlexAccessToken "$PLEX_TOKEN"
 xmlstarlet ed -u "/Preferences/@PlexOnlineToken" -v "$PLEX_TOKEN" ~/snap/plex-desktop/common/Plex Media Server/Preferences.xml >tmp.xml && mv tmp.xml Preferences.xml
+echo "Maybe you need to add the PLEX_AUTH_TOKEN: $PLEX_AUTH_TOKEN to the user json field in the plex.ini file"
