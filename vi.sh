d9c71ed4d264885a11ceb00b654a6ddd918e742c
DanielHauge <animcuil@gmail.com>
2025-07-09 21:47:58 +0200

    fix: fixed sudo not in containers

--- vi.sh
+++ vi.sh
@@ -11,6 +11,12 @@ else
     TMPBASE="/tmp"
 fi
 
+# check if sudo
+if command -v sudo &>/dev/null; then
+    sudo "$0" "$@"
+    exit 0
+fi
+
 # ensure git is installed otherwise install it based on the OS
 if ! command -v git &>/dev/null; then
     echo "git is not installed. Installing git..."
@@ -19,27 +25,27 @@ if ! command -v git &>/dev/null; then
     case "$OS" in
     Linux)
         if command -v apt-get &>/dev/null; then
-            sudo apt-get update && sudo apt-get install -y git
+            apt-get update && sudo apt-get install -y git
         elif command -v yum &>/dev/null; then
-            sudo yum install -y git
+            yum install -y git
         elif command -v dnf &>/dev/null; then
-            sudo dnf install -y git
+            dnf install -y git
         elif command -v pacman &>/dev/null; then
-            sudo pacman -Syu git
+            pacman -Syu git
         elif command -v zypper &>/dev/null; then
-            sudo zypper install -y git
+            zypper install -y git
         elif command -v apk &>/dev/null; then
-            sudo apk add git
+            apk add git
         elif command -v emerge &>/dev/null; then
-            sudo emerge --ask dev-vcs/git
+            emerge --ask dev-vcs/git
         elif command -v xbps-install &>/dev/null; then
-            sudo xbps-install -S git
+            xbps-install -S git
         elif command -v brew &>/dev/null; then
             brew install git
         elif command -v flatpak &>/dev/null; then
             flatpak install flathub org.gnome.gitg
         elif command -v snap &>/dev/null; then
-            sudo snap install git --classic
+            snap install git --classic
         else
             echo "Unsupported Linux distribution. Please install git manually."
             exit 1
