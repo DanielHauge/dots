83e4ba505dab44534748154d5bc5630982ea9217
DanielHauge <animcuil@gmail.com>
2025-07-09 22:27:44 +0200

    feat: supress warnings and errors in minimal mode

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -1,4 +1,19 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+if vim.env.NVIM_MINIMAL == "1" then
+  -- Make all messages silent
+  vim.opt.shortmess:append("a") -- avoid "written" messages, etc.
+  vim.opt.cmdheight = 0 -- no command line
+  vim.opt.showmode = false
+  vim.opt.laststatus = 0 -- hide status line
+  vim.opt.ruler = false -- hide ruler
+  vim.opt.showcmd = false -- hide command preview
+
+  -- Completely suppress errors/warnings/info printed by plugins
+  vim.notify = function(...) end
+  vim.api.nvim_err_writeln = function(...) end
+  vim.api.nvim_echo = function(...) end
+  vim.api.nvim_notify = function(...) end
+end
 
 require("config.highlights")
 require("config.lazy")
