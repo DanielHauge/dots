bc85d27f08da6269cc379db671c4b4c2f27c07d5
DanielHauge <animcuil@gmail.com>
2025-07-09 21:45:48 +0200

    fix: no sudo in containers

--- quick-init/vi.sh
+++ quick-init/vi.sh
@@ -18,32 +18,43 @@ XDG_DATA_HOME="$(mktemp -d "$TMPBASE/daniel-nvim-data.XXXXXX")"
 export XDG_DATA_HOME
 XDG_CONFIG_HOME="$DOTS_LOC/config/.config"
 
+# if sudo is a command, then do sudo for the session
+if command -v sudo &>/dev/null; then
+    if [[ "$EUID" -ne 0 ]]; then
+        echo "Running with sudo..."
+        # use just sudo to quickly elevate privileges
+        sudo -E "$0" "$@"
+    fi
+else
+    echo "Warning: 'sudo' command not found. Running without elevated privileges."
+fi
+
 # install neovim based on the os
 OS=$(uname -s)
 # if linux
 if [[ "$OS" == "Linux" ]]; then
     if command -v apt-get &>/dev/null; then
-        sudo apt-get update && sudo apt-get install -y neovim
+        apt-get update && sudo apt-get install -y neovim
     elif command -v yum &>/dev/null; then
-        sudo yum install -y neovim
+        yum install -y neovim
     elif command -v dnf &>/dev/null; then
-        sudo dnf install -y neovim
+        dnf install -y neovim
     elif command -v pacman &>/dev/null; then
-        sudo pacman -Syu neovim
+        pacman -Syu neovim
     elif command -v zypper &>/dev/null; then
-        sudo zypper install -y neovim
+        zypper install -y neovim
     elif command -v apk &>/dev/null; then
-        sudo apk add neovim
+        apk add neovim
     elif command -v emerge &>/dev/null; then
-        sudo emerge --ask app-editors/neovim
+        emerge --ask app-editors/neovim
     elif command -v xbps-install &>/dev/null; then
-        sudo xbps-install -S neovim
+        xbps-install -S neovim
     elif command -v brew &>/dev/null; then
         brew install neovim
     elif command -v flatpak &>/dev/null; then
         flatpak install flathub org.neovim.nvim
     elif command -v snap &>/dev/null; then
-        sudo snap install nvim --classic
+        snap install nvim --classic
     else
         echo "Unsupported Linux distribution. Please install neovim manually."
         exit 1
