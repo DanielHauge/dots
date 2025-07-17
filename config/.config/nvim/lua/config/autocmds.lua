db22c23a8c89ae42a68f75b2eddbf503038bb136
DanielHauge <animcuil@gmail.com>
2025-07-17 22:48:40 +0200

    fix: Fixed small issues with h5v integration

--- config/.config/nvim/lua/config/autocmds.lua
+++ config/.config/nvim/lua/config/autocmds.lua
@@ -15,17 +15,34 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+vim.api.nvim_create_autocmd({ "BufReadCmd" }, {
   pattern = { "*.h5", "*.hdf5", "*.hdf", "*.pg3" },
   callback = function(args)
     local file = vim.fn.expand("%:p")
 
     -- Delete the buffer before it loads
     vim.schedule(function()
+      vim.cmd("bdelete! " .. args.buf) -- DIdnt work
       vim.cmd("enew") -- Open an empty buffer
-      vim.cmd("bdelete " .. args.buf) -- DIdnt work
       vim.cmd("terminal h5v " .. vim.fn.fnameescape(file))
       vim.cmd("startinsert")
     end)
+  end,
+})
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+  pattern = "*",
+  callback = function()
+    -- Check if we just left terminal insert mode
+    if vim.fn.mode() == "n" then
+      local buftype = vim.bo.buftype
+      if buftype == "terminal" then
+        -- Move cursor to last line and first column
+        local last_line = vim.api.nvim_buf_line_count(0)
+        vim.api.nvim_win_set_cursor(0, { last_line, 0 })
+        -- Scroll to bottom
+        vim.cmd("normal! zb")
+      end
+    end
   end,
 })
