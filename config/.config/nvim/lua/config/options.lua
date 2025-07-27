a87de90e28d5e6877af5b759e228bb00ff6d7a4f
DanielHauge <animcuil@gmail.com>
2025-07-27 11:05:26 +0200

    feat: Disabled vim sawp

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -11,6 +11,7 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.swapfile = false
 
 vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
