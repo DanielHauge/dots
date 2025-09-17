8b95d7e0f1586971af87553c314bc1df5ed1cfe8
DanielHauge <animcuil@gmail.com>
2025-09-17 20:24:15 +0200

    fix: fixed mergeconflics

---
+++ config/.config/nvim/lua/plugins/jupyter.lua
@@ -0,0 +1,38 @@
+vim.filetype.add({
+  extension = {
+    ipynb = "ipynb",
+  },
+})
+
+return {
+  {
+    "benlubas/molten-nvim",
+    version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+    dependencies = { "3rd/image.nvim" },
+    build = ":UpdateRemotePlugins",
+    init = function()
+      -- these are examples, not defaults. Please see the readme
+      vim.g.molten_image_provider = "image.nvim"
+      vim.g.molten_output_win_max_height = 20
+    end,
+    ft = { "ipynb" },
+  },
+  {
+    -- see the image.nvim readme for more information about configuring this plugin
+    "3rd/image.nvim",
+    opts = {
+      backend = "kitty", -- whatever backend you would like to use
+      max_width = 100,
+      max_height = 12,
+      max_height_window_percentage = math.huge,
+      max_width_window_percentage = math.huge,
+      window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+    },
+  },
+
+  -- pipx install pynvim
+  -- pipx install jupyter
+  -- pipx install jupyter-client
+  -- pipx runpip pynvim install jupyter_client
+}
