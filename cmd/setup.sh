371f1aa4a1a2f7c59d6827e7686a5399dbc42fe9
DanielHauge <animcuil@gmail.com>
2025-10-25 17:40:56 +0200

    feat: Improved new custom cmds workings

--- cmd/setup.sh
+++ cmd/setup.sh
@@ -1,4 +1,6 @@
 export PATH="$PATH:$DOTS_LOC/cmd/bin"
-for fn_file in "$DOTS_LOC/cmd/fn/"*.sh; do
+for fn_file in "$DOTS_LOC/cmd/fn/"*; do
     [ -e "$fn_file" ] && source "$fn_file"
 done
+
+source "$DOTS_LOC/cmd/aliases.sh"
