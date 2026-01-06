6c2fc2d74a1723d2241469b491ba6deab56f85f7
DanielHauge <animcuil@gmail.com>
2026-01-06 18:41:41 +0100

    feat: Added black and flashusb

--- arch/plex.sh
+++ arch/plex.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-paru -Syuu snadp
-sudo systemctl enable --now snapd.socket
-sudo snap install plex-desktop
-crudini --set ~/snap/plex-desktop/common/plex.ini web myPlexAccessToken "$PLEX_TOKEN"
-xmlstarlet ed -u "/Preferences/@PlexOnlineToken" -v "$PLEX_TOKEN" ~/snap/plex-desktop/common/"Plex Media Server"/Preferences.xml >tmp.xml && mv tmp.xml Preferences.xml
+paru -Syuu plex-desktop
+# sudo snap install plex-desktop
+# PLEX_LOC="~/.local/share/plex"
+PLEX_LOC="$HOME/snap/plex-desktop/common"
+
+crudini --set "$PLEX_LOC"/plex.ini web myPlexAccessToken "$PLEX_TOKEN"
+xmlstarlet ed -u "/Preferences/@PlexOnlineToken" -v "$PLEX_TOKEN" "$PLEX_LOC"/"Plex Media Server"/Preferences.xml >tmp.xml && mv tmp.xml ~/.local/share/plex/"Plex Media Server"/Preferences.xml
 echo "Maybe you need to add the PLEX_AUTH_TOKEN: $PLEX_AUTH_TOKEN to the user json field in the plex.ini file"
