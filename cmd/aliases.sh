0a5bddd320e55c2243457d142c9eb5371b3c3d27
DanielHauge <animcuil@gmail.com>
2026-07-15 22:31:08 +0200

    feat: Added opencode configs

--- cmd/aliases.sh
+++ cmd/aliases.sh
@@ -26,6 +26,7 @@ alias repo="cd $REPO_DIR"
 alias rpeo="cd $REPO_DIR" # typo
 alias suvi='sudo -E nvim'
 alias vi='nvim'
+alias oc='opencode'
 alias cp='cpv --progress'
 alias vim='nvim'
 alias lg='lazygit'
