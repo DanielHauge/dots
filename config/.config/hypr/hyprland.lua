43b712b92baa7f1fd0c5798b8d21f8bbda962c38
DanielHauge <animcuil@gmail.com>
2026-08-22 11:29:24 +0200

    feat: Fixed keybinds to resize windows

--- config/.config/hypr/hyprland.lua
+++ config/.config/hypr/hyprland.lua
@@ -12,42 +12,56 @@ local editor = "nvim"
 local browser = "firefox"
 
 hl.on("hyprland.start", function()
-    hl.exec_cmd("waybar")
-    hl.exec_cmd("discord")
-    hl.exec_cmd("udiskie")
-    hl.exec_cmd("waypaper --random --fill fill --folder /mnt/nas/wallpapers/")
-    hl.exec_cmd("nm-applet")
-    hl.exec_cmd("walker --gapplication-service")
-    hl.exec_cmd("elephant")
-    hl.exec_cmd("kdeconnectd")
-    hl.exec_cmd("kdeconnect-indicator")
-    hl.exec_cmd("swaync")
-    hl.exec_cmd("mm rss")
+	hl.exec_cmd("waybar")
+	hl.exec_cmd("discord")
+	hl.exec_cmd("udiskie")
+	hl.exec_cmd("waypaper --random --fill fill --folder /mnt/nas/wallpapers/")
+	hl.exec_cmd("nm-applet")
+	hl.exec_cmd("walker --gapplication-service")
+	hl.exec_cmd("elephant")
+	hl.exec_cmd("kdeconnectd")
+	hl.exec_cmd("kdeconnect-indicator")
+	hl.exec_cmd("swaync")
+	hl.exec_cmd("mm rss")
 end)
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 
 hl.config({
-    general = {
-        gaps_in = 5, gaps_out = 20, border_size = 2,
-        col = { active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 }, inactive_border = "rgba(595959aa)" },
-        resize_on_border = false, allow_tearing = false, layout = "dwindle",
-    },
-    decoration = {
-        rounding = 15, active_opacity = 1.0,
-        shadow = { enabled = true, range = 4, render_power = 3, color = 0xee1a1a1a },
-        blur = { enabled = true, size = 3, passes = 1, vibrancy = 0.1696 },
-    },
-    animations = { enabled = true },
-    dwindle = { preserve_split = true },
-    master = { new_status = "master" },
-    misc = { force_default_wallpaper = -1, disable_hyprland_logo = false },
-    input = {
-        kb_layout = "dk", kb_variant = "", kb_model = "", kb_options = "", kb_rules = "",
-        follow_mouse = 1, sensitivity = 0, accel_profile = "flat",
-        touchpad = { natural_scroll = true },
-    },
+	general = {
+		gaps_in = 5,
+		gaps_out = 20,
+		border_size = 2,
+		col = {
+			active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
+			inactive_border = "rgba(595959aa)",
+		},
+		resize_on_border = false,
+		allow_tearing = false,
+		layout = "dwindle",
+	},
+	decoration = {
+		rounding = 15,
+		active_opacity = 1.0,
+		shadow = { enabled = true, range = 4, render_power = 3, color = 0xee1a1a1a },
+		blur = { enabled = true, size = 3, passes = 1, vibrancy = 0.1696 },
+	},
+	animations = { enabled = true },
+	dwindle = { preserve_split = true },
+	master = { new_status = "master" },
+	misc = { force_default_wallpaper = -1, disable_hyprland_logo = false },
+	input = {
+		kb_layout = "dk",
+		kb_variant = "",
+		kb_model = "",
+		kb_options = "",
+		kb_rules = "",
+		follow_mouse = 1,
+		sensitivity = 0,
+		accel_profile = "flat",
+		touchpad = { natural_scroll = true },
+	},
 })
 
 hl.device({ name = "epic-mouse-v1", sensitivity = 1 })
@@ -92,7 +106,12 @@ hl.bind(mainMod .. " + P", hl.dsp.exec_cmd(packages))
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd(symbols))
 hl.bind(mainMod .. " + U", hl.dsp.exec_cmd(unicodes))
 hl.bind(mainMod .. " + F", hl.dsp.exec_cmd(files))
-hl.bind(mainMod .. " + S", hl.dsp.exec_cmd([[pkill slurp || hyprshot -m ${1:-region} --raw | satty --filename - --early-exit --actions-on-enter save-to-clipboard --copy-command 'wl-copy']]))
+hl.bind(
+	mainMod .. " + S",
+	hl.dsp.exec_cmd(
+		[[pkill slurp || hyprshot -m ${1:-region} --raw | satty --filename - --early-exit --actions-on-enter save-to-clipboard --copy-command 'wl-copy']]
+	)
+)
 hl.bind(mainMod .. " + SHIFT + R", hl.dsp.exec_cmd("clip-rec region"))
 hl.bind(mainMod .. " + CTRL + R", hl.dsp.exec_cmd("clip-rec full"))
 hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))
@@ -104,9 +123,9 @@ hl.bind(mainMod .. " + up", hl.dsp.focus({ direction = "up" }))
 hl.bind(mainMod .. " + down", hl.dsp.focus({ direction = "down" }))
 
 for i = 1, 10 do
-    local key = i % 10
-    hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
-    hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
+	local key = i % 10
+	hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
+	hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
 end
 
 hl.bind(mainMod .. " + Z", hl.dsp.workspace.toggle_special("magic"))
@@ -121,17 +140,37 @@ hl.bind(mainMod .. " + SHIFT + left", hl.dsp.window.move({ direction = "left" })
 hl.bind(mainMod .. " + SHIFT + H", hl.dsp.window.move({ direction = "left" }))
 hl.bind(mainMod .. " + SHIFT + right", hl.dsp.window.move({ direction = "right" }))
 hl.bind(mainMod .. " + SHIFT + L", hl.dsp.window.move({ direction = "right" }))
-hl.bind(mainMod .. " + CTRL + left", hl.dsp.window.resize({ x = -60, y = 0 }))
-hl.bind(mainMod .. " + CTRL + right", hl.dsp.window.resize({ x = 60, y = 0 }))
-hl.bind(mainMod .. " + CTRL + up", hl.dsp.window.resize({ x = 0, y = -60 }))
-hl.bind(mainMod .. " + CTRL + down", hl.dsp.window.resize({ x = 0, y = 60 }))
+hl.bind(mainMod .. " + CTRL + left", hl.dsp.window.resize({ x = -60, y = 0, relative = true }))
+hl.bind(mainMod .. " + CTRL + right", hl.dsp.window.resize({ x = 60, y = 0, relative = true }))
+hl.bind(mainMod .. " + CTRL + up", hl.dsp.window.resize({ x = 0, y = -60, relative = true }))
+hl.bind(mainMod .. " + CTRL + down", hl.dsp.window.resize({ x = 0, y = 60, relative = true }))
+hl.bind(mainMod .. " + ALT + left", hl.dsp.window.resize({ x = -200, y = 0, relative = true }))
+hl.bind(mainMod .. " + ALT + right", hl.dsp.window.resize({ x = 200, y = 0, relative = true }))
+hl.bind(mainMod .. " + ALT + up", hl.dsp.window.resize({ x = 0, y = -200, relative = true }))
+hl.bind(mainMod .. " + ALT + down", hl.dsp.window.resize({ x = 0, y = 200, relative = true }))
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"), { locked = true, repeating = true })
-hl.bind("XF86AudioMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"), { locked = true, repeating = true })
-hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"), { locked = true, repeating = true })
+hl.bind(
+	"XF86AudioRaiseVolume",
+	hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86AudioLowerVolume",
+	hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86AudioMute",
+	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86AudioMicMute",
+	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
+	{ locked = true, repeating = true }
+)
 hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl s 10%+"), { locked = true, repeating = true })
 hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl s 10%-"), { locked = true, repeating = true })
 hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
